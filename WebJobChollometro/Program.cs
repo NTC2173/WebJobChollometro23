de45bdb62bbbba2b3dacdb047b652d260746507d
Nacho_21 <igntc93@gmail.com>
2023-02-16 19:53:11 +0100

    Add project files.

---
+++ WebJobChollometro/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebJobChollometro.Data;
+using WebJobChollometro.Repositories;
+
+string connectionString =
+    @"Data Source=sqleoinacho.database.windows.net;Initial Catalog=EOIAZURE;User ID=adminsql;Password=Admin123";
+
+var provider =
+    new ServiceCollection()
+    .AddTransient<RepositoryChollos>()
+    .AddDbContext<ChollometroContext>
+    (options => options.UseSqlServer(connectionString))
+    .BuildServiceProvider();
+//RECUPERAMOS NUESTRO REPOSITORY UTILIZANDO EL PROVIDER
+RepositoryChollos repo = provider.GetService<RepositoryChollos>();
+Console.WriteLine("Pulse ENTER para iniciar");
+Console.ReadLine();
+repo.PopulateChollos();
+Console.WriteLine("Proceso completado.  Pulse ENTER para finalizar.");
+Console.ReadLine();
+
